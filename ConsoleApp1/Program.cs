6fa247c3a8e7ce49a7b673aae81b07e962bb454a
Grandswed1 <elliot.rydell@elev.ga.ntig.se>
2024-09-05 10:52:08 +0200

    BashAMonster

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿Megaman man = new Megaman() 
+{
+    Name = "Rockman",
+    Hitpoints = 100
+};
+
+Megaman zero = new Megaman()
+{
+    Name = "Zero",
+    Hitpoints = 100
+};
+
+Console.WriteLine(man.Name);
+Console.ReadLine();
